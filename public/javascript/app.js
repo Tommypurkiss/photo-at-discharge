1b60f24778ea1a178e6ce888c78b4c63dd9817fa
Tommy Purkiss <tommypurkiss1@gmail.com>
2019-11-01 22:25:15 +0000

    generate letter almost complete

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -30,22 +30,86 @@ function woundPhotoCheck() {
 // PDF
 
 function generatePDF() {
-
-    var test = document.getElementById('maple-ward').value;
-
-    var testImg = document.getElementById('wound-photo-one-img');
-    // var img1 = document.createElement("img");
-    // img1.src = "public\assets\img\wound-imgs\Mr M. Abbass 4074554 (blurred).jpg";
-
-    var options = {unit: 'px', format: 'a4'};
-
-    var doc = new jsPDF(options);
-
-    doc.text('Hello world!', 20, 100);
     //doc.addPage(); //generates a new page
-    doc.text(test, 10, 20);
-    doc.addImage(testImg, 'JPEG', 100 , 120, 200, 300); //works kinda
-    //doc.viewerPreferences({'FitWindow': true}, true) //probs dont need
 
+
+    // New Doc
+    var options = {unit: 'px', format: 'a4'};
+    var doc = new jsPDF(options);
+    doc.setFontSize(10);
+
+    // Ward title
+    // var ward = document.getElementById('ward-options').value;
+    // doc.text(ward, 10, 20);
+
+    // NHS title logo
+    //doc.text('Royal Brompton & Harefield | NHS', 245, 30);
+
+    // Wound photo img
+    // if (document.getElementById('wound-photo-one').checked == true) {
+    //     var checkedImg = document.getElementById('wound-photo-one-img');
+    //     doc.addImage(checkedImg, 'JPEG', 280, 100, 150, 300);
+    // } 
+    // else if (document.getElementById('wound-photo-two').checked == true) {
+    //     var checkedImg = document.getElementById('wound-photo-two-img');
+    //     doc.addImage(checkedImg, 'JPEG', 280, 100, 150, 300);
+    // } 
+    // else if (document.getElementById('wound-photo-three').checked == true) {
+    //     checkedImg = document.getElementById('wound-photo-three-img');
+    //     doc.addImage(checkedImg, 'JPEG', 280, 100, 150, 300);
+    // }
+    // else if (document.getElementById('wound-photo-four').checked == true) {
+    //     checkedImg = document.getElementById('wound-photo-four-img');
+    //     doc.addImage(checkedImg, 'JPEG', 280, 100, 150, 300);
+    // }
+    // else if (document.getElementById('wound-photo-five').checked == true) {
+    //     checkedImg = document.getElementById('wound-photo-five-img');
+    //     doc.addImage(checkedImg, 'JPEG', 280, 100, 150, 300);
+    // }
+
+    // Correct img 
+    checkedImg = document.getElementById('wound-photo-four-img');
+    doc.addImage(checkedImg, 'JPEG', 280, 100, 150, 300);
+
+
+    doc.text('Mr J Blogg XX-XXX', 20, 80);
+    doc.text('NHS NUMBER XXX XXX XXX', 20, 90);
+
+    // doc.fromHTML($('#p1').get(0), 20, 92, {
+    //     'width': 250,
+        
+    // });
+
+    doc.fromHTML($('#left-text').get(0), 20, 92, {
+        'width': 250,
+        
+    });
+
+    doc.fromHTML($('#bottom-text').get(0), 20, 410, {
+        'width': 400,
+        
+    });
+
+    doc.fromHTML($('#bottom-sign').get(0), 20, 590, {
+        'width': 400,
+        
+    });
+
+    // doc.fromHTML($('#p2').get(0), 20, 135, {
+    //     'width': 250
+    // });
+
+    // doc.fromHTML($('#p3').get(0), 20, 205, {
+    //     'width': 250
+    // });
+
+
+    // doc.text()
+
+    // doc.fromHTML($('#pdf').get(0), 20, 20, {
+    //     'width' : 500
+    // });
+
+    // Save pdf
     doc.save('Photo-at-Discharge.pdf');
 }
