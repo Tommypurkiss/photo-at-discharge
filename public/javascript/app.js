ecbe3b8919522172cd2302b4033d05254858fba1
Tommy Purkiss <tommypurkiss1@gmail.com>
2019-11-04 19:58:08 +0000

    end date should be working after giving starting date

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -1,46 +1,14 @@
 
 
-// enter current date for first datepicker
 
 
-// var date = new Date();
-// console.log(date);
+// var today = new Date();
+// var dd = String(today.getDate())
+// var mm = String(today.getMonth() + 1) //January is 0!
+// var yyyy = today.getFullYear();
 
-// var day = date.getDate();
-// var month = date.getMonth() + 1;
-// var year = date.getFullYear();
-
-// // if (month < 10) month = "0" + month;
-// // if (day < 10) day = "0" + day;
-
-// var today = year + "-" + month + "-" + day;       
-
-// let startDateString = document.getElementById("start-date");
-// startDateString.value = "";
-// console.log(startDateString);
-
-
-// function setDate() {
-
-//     let date = new Date()
-
-//     let day = String(date.getDate())
-//     let month = String(date.getMonth())
-//     let year = String(date.getFullYear())
-
-//     let finalDate = day + "/" + month + "/" + year
-//     console.log(finalDate)
-    
-// }
-
-
-var today = new Date();
-var dd = String(today.getDate())
-var mm = String(today.getMonth() + 1) //January is 0!
-var yyyy = today.getFullYear();
-
-today = dd + '/' + mm + '/' + yyyy;
-console.log(today)
+// today = dd + '/' + mm + '/' + yyyy;
+// console.log(today)
 
 
 // checking which photo is chosen
@@ -68,6 +36,25 @@ function woundPhotoCheck() {
     }
     //document.getElementById('wound-photo-answer').innerHTML = "" + photoValue;
 }
+
+
+function saveAndExit() {
+
+    // check wound assessment textbox for word scratch
+
+    // var woundAssessmentText = document.getElementById('#wound-assessment-text')
+
+    // woundAssessmentText.innerHTML() = "";
+    // //let scratchWord = "scratch"
+    // //var scratch = "";
+    // var scratchWord = woundAssessmentText.includes("scratch");
+
+    // document.getElementById('#wound-assessment-text').innerHTML = scratchWord;
+
+    // // if ()
+}
+
+
 
 
 
--- public/javascript/jquery.js
+++ public/javascript/jquery.js
@@ -1,4 +1,5 @@
-
+// $(document).ready(function(){
+// });
 
 // WOUND IMG UPLOAD
 //  $(document).ready(function(){
@@ -34,14 +35,87 @@ $(document).ready(function(){
 
 // RETRIEVE DATE VALUE FROM DATE PICKER
 
+// $(document).ready(function(){
+
+
+
+
+//     $('#start-date-submit').on('click', function(){
+
+//         // the set date from input 
+//         var date = new Date($('#start-date').val());
+//         day = date.getDate();
+//         month = date.getMonth() + 1;
+//         year = date.getFullYear();
+//         var setDate = day + "/" + month + "/" + year
+//         console.log(setDate)
+//         //alert([day, month, year].join('/'));
+
+
+
+//         // the current date
+//         var currentDate = new Date();
+
+//         var dd = String(currentDate.getDate())
+//         var mm = String(currentDate.getMonth() + 1) //January is 0!
+//         var yyyy = currentDate.getFullYear();
+
+//         currentDate = dd + '/' + mm + '/' + yyyy;
+//         console.log(currentDate)
+
+//         if (setDate != currentDate) {
+//             alert("Please choose todays date")
+//             $('#start-date').val("");
+//         }
+//         else {
+//             alert("Today's date has been set")
+//         }
+
+//     });
+
+//     // $('#end-date-submit').on('click', function(){
+        
+        
+        
+
+//     // });
+
+
+//     $('#end-date-submit').on('click', function(){
+        
+//         // var date2 = new Date($('#end-date').val());
+//         // day2 = date2.getDate() + 7;
+//         // month2 = date2.getMonth() + 1;
+//         // year2 = date2.getFullYear();
+//         // var setEndDate = day2 + "/" + month2 + "/" + year2
+//         // console.log(setEndDate)
+
+//         var endDate = setDate.getDate() +7
+//         console.log(endDate)
+
+//     });
+
+// });
+
+
+
+
+
+
+// NEW DATE TRY 2
+
 $(document).ready(function(){
+
+
+
+    
     $('#start-date-submit').on('click', function(){
 
         // the set date from input 
         var date = new Date($('#start-date').val());
-        day = date.getDate();
-        month = date.getMonth() + 1;
-        year = date.getFullYear();
+        var day = date.getDate();
+        var month = date.getMonth() + 1;
+        var year = date.getFullYear();
         var setDate = day + "/" + month + "/" + year
         console.log(setDate)
         //alert([day, month, year].join('/'));
@@ -62,7 +136,103 @@ $(document).ready(function(){
             alert("Please choose todays date")
             $('#start-date').val("");
         }
+        else {
+            alert("Today's date has been set")
+        }
 
     });
+
+    // $('#end-date-submit').on('click', function(){
+        
+        
+        
+
+    // });
+
+
+    $('#end-date-submit').on('click', function(){
+        
+        // var date2 = new Date($('#end-date').val());
+        // day2 = date2.getDate() + 7;
+        // month2 = date2.getMonth() + 1;
+        // year2 = date2.getFullYear();
+        // var setEndDate = day2 + "/" + month2 + "/" + year2
+        // console.log(setEndDate)
+
+        
+
+
+
+        // the set date from input 
+        var date = new Date($('#end-date').val());
+        var day = date.getDate();
+        var month = date.getMonth() + 1;
+        var year = date.getFullYear();
+        var setDate = day + "/" + month + "/" + year
+        console.log(setDate)
+
+
+
+        // the current date
+        var currentDate = new Date();
+
+        var dd = String(currentDate.getDate() + 7)
+        var mm = String(currentDate.getMonth() + 1) //January is 0!
+        var yyyy = currentDate.getFullYear();
+
+        currentDate = dd + '/' + mm + '/' + yyyy;
+        console.log(currentDate)
+
+        if (setDate != currentDate) {
+            alert("Please choose the starting date first. The end date should be a week after the starting set date")
+            $('#end-date').val("");
+        }
+        else {
+            alert("End date has been set")
+        }
+        
+
+
+
+    });
+
 });
 
+
+
+
+// END NEW DATE TRY 2
+
+
+
+
+
+
+
+
+// $(document).ready(function(){
+
+//     $('#end-date-submit').on('click', function(){
+        
+//         var date2 = new Date($('#end-date').val());
+//         day2 = date2.getDate();
+//         month2 = date2.getMonth() + 1;
+//         year2 = date2.getFullYear();
+//         var setEndDate = day2 + "/" + month2 + "/" + year2
+//         console.log(setEndDate)
+
+
+//     });
+
+    
+
+// });
+
+
+
+// $(document).ready(function(){
+
+//     var woundAssessmentText = $('#wound-assessment-text');
+//     var scratchWord = "scratch"
+
+// });
