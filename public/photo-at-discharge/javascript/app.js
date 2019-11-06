17bbb985c5228b43e52465a7189b9a8c2cf4e02a
Tommy <tommypurkiss1@gmail.com>
2019-11-06 13:53:11 +0000

    form changes removing select and some other updates

--- public/photo-at-discharge/javascript/app.js
+++ public/photo-at-discharge/javascript/app.js
@@ -1,7 +1,4 @@
-
-
-
-
+// Date 
 var today = new Date();
 var dd = String(today.getDate())
 var mm = String(today.getMonth() + 1) //January is 0!
@@ -11,31 +8,181 @@ today = dd + '/' + mm + '/' + yyyy;
 console.log(today)
 
 
-// checking which photo is chosen
-function woundPhotoCheck() {
+// function pdfValues() {
+    
+
+//     p1 = document.getElementById('p1');
+
+//     dischargeDate = document.getElementById('start-date').required.value
+//     console.log("dischargeDate", dischargeDate)
+    
+//     normalSwellingText = "Normal swelling at the top of the wound which will resolve over a few weeks."
+//     console.log("normalSwellingText", normalSwellingText)
+
+//     woundDropdown = document.getElementById('wound-dropdown').value
+//     console.log("woundDropdown", woundDropdown)
+
+//     drainSitesDropdown = document.getElementById('drain-sites-dropdown').value
+//     console.log("drainSitesDropdown", drainSitesDropdown)
+
+//     removalDate = document.getElementById('end-date').value
+//     console.log("removalDate", removalDate)
+
+//     legWoundsDropdown = document.getElementById('leg-wounds-dropdown').value
+//     console.log("legWoundsDropdown", legWoundsDropdown)
+
+//     p1.innerHTML = dischargeDate + " " + microValue 
+//             + " " + normalSwellingText + " " + woundDropdown 
+//             + " " + drainSitesDropdown + " " + removalDate
+//             + " " + legWoundsDropdown + " " + removalDate
+
+//     if ((dischargeDate === undefined) ) {
+//         alert("Please enter a date")
+//         console.log(" date undefined", dischargeDate)
+//     }
+//     else {
+//         //return false
+//         generatePDF()
+//     }
+
+
+// }
+
+// Generate the PDF
+function generatePDF() {
+
+    checkWoundImages() //function for checking wound images
+    checkMicroForm() //function for checking micro wound radio options
+    //pdfValues()
+
+
+    p1 = document.getElementById('p1');
+
+    dischargeDate = document.getElementById('start-date').required.value
+    console.log("dischargeDate", dischargeDate)
+    
+    normalSwellingText = "Normal swelling at the top of the wound which will resolve over a few weeks."
+    console.log("normalSwellingText", normalSwellingText)
+
+    woundDropdown = document.getElementById('wound-dropdown').value
+    console.log("woundDropdown", woundDropdown)
+
+    drainSitesDropdown = document.getElementById('drain-sites-dropdown').value
+    console.log("drainSitesDropdown", drainSitesDropdown)
+
+    removalDate = document.getElementById('end-date').value
+    console.log("removalDate", removalDate)
+
+    legWoundsDropdown = document.getElementById('leg-wounds-dropdown').value
+    console.log("legWoundsDropdown", legWoundsDropdown)
+
+
+    if ((dischargeDate == "Nan/NaN/NaN") ) {
+        alert("Please enter a date")
+        console.log(" date undefined", dischargeDate)
+    }
+
+    // if ((dischargeDate === undefined) ) {
+    //     alert("Please enter a date")
+    //     console.log(" date undefined", dischargeDate)
+    // }
+    // else {
+    //     return true
+    //     //generatePDF()
+    // }
+
+
+    p1.innerHTML = dischargeDate + " " + microValue 
+            + " " + normalSwellingText + " " + woundDropdown 
+            + " " + drainSitesDropdown + " " + removalDate
+            + " " + legWoundsDropdown + " " + removalDate
+
+    // New Doc
+    var options = {unit: 'px', format: 'a4'};
+    var doc = new jsPDF(options);
+    doc.setFontSize(10);
+
+    doc.text('Mr J Blogg XX-XXX', 20, 80);
+    doc.text('NHS NUMBER XXX XXX XXX', 20, 90);
+
+    doc.addImage(checkedImg, 'JPEG', 280, 100, 150, 300);
+
+    doc.fromHTML($('#left-text').get(0), 20, 92, {
+        'width': 250,        
+    });
+
+    doc.fromHTML($('#bottom-text').get(0), 20, 410, {
+        'width': 400,
+    });
+
+    doc.fromHTML($('#bottom-sign').get(0), 20, 590, {
+        'width': 400,
+    });
+
+    // Save pdf
+    doc.save('Photo-at-Discharge.pdf');
+}
+// End PDF
+
+
+//works - TODO: might not be necessary 
+function checkDrName() {
+
+    let docTextbox = document.getElementById('doctor-name')
+
+    if ((docTextbox).value == "") {
+        alert('Please enter a doctors name')
+    }
+}
+
+function checkWoundImages() {
 
     if (document.getElementById('wound-photo-one').checked == true) {
-        photoValue = document.getElementById('wound-photo-one').value;
-        alert(photoValue);
+        checkedImg = document.getElementById('wound-photo-one-img');
     } 
     else if (document.getElementById('wound-photo-two').checked == true) {
-        photoValue = document.getElementById('wound-photo-two').value;
-        alert(photoValue);
+        checkedImg = document.getElementById('wound-photo-two-img');
     } 
     else if (document.getElementById('wound-photo-three').checked == true) {
-        photoValue = document.getElementById('wound-photo-three').value;
-        alert(photoValue);
+        checkedImg = document.getElementById('wound-photo-three-img');
     }
     else if (document.getElementById('wound-photo-four').checked == true) {
-        photoValue = document.getElementById('wound-photo-four').value;
-        alert(photoValue);
+        checkedImg = document.getElementById('wound-photo-four-img');
     }
     else if (document.getElementById('wound-photo-five').checked == true) {
-        photoValue = document.getElementById('wound-photo-five').value;
-        alert(photoValue);
+        checkedImg = document.getElementById('wound-photo-five-img');
     }
-    //document.getElementById('wound-photo-answer').innerHTML = "" + photoValue;
 }
+
+function checkMicroForm() {
+
+    if (document.getElementById('micro-one').checked == true) {
+        microValue = document.getElementById('micro-one').value
+    } 
+    else if (document.getElementById('micro-two').checked == true) {
+        microValue = document.getElementById('micro-two').value
+    } 
+    else if (document.getElementById('micro-three').checked == true) {
+        microValue = document.getElementById('micro-two').value
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* SPARE CODE
+
+
+
 
 
 function saveAndExit() {
@@ -56,7 +203,7 @@ function saveAndExit() {
 
     //checkDrName(); //works
 
-    checkWord()
+    //checkWord()
 }
 
 
@@ -98,18 +245,9 @@ function checkWord() {
 }
 
 
-// PDF
 
-function generatePDF() {
-    //doc.addPage(); //generates a new page
-    checkWoundImages() //function for checking wound images
-    checkMicroForm() //function for checking micro wound radio options
 
-    // New Doc
-    var options = {unit: 'px', format: 'a4'};
-    var doc = new jsPDF(options);
-    doc.setFontSize(10);
-
+    // generatePDF()
     // Ward title
     // var ward = document.getElementById('ward-options').value;
     // doc.text(ward, 10, 20);
@@ -123,134 +261,42 @@ function generatePDF() {
     // let year = dischargeDate.getFullYear()
 
 
-    let p1 = document.getElementById('p1');
+    // let p1 = document.getElementById('p1');
 
-    let dischargeDate = document.getElementById('start-date').value
-    console.log("dischargeDate", dischargeDate)
+    // let dischargeDate = document.getElementById('start-date').value
+    // console.log("dischargeDate", dischargeDate)
     
-    let normalSwellingText = "Normal swelling at the top of the wound which will resolve over a few weeks."
-    console.log("normalSwellingText", normalSwellingText)
+    // let normalSwellingText = "Normal swelling at the top of the wound which will resolve over a few weeks."
+    // console.log("normalSwellingText", normalSwellingText)
 
-    let woundDropdown = document.getElementById('wound-dropdown').value
-    console.log("woundDropdown", woundDropdown)
-    // let woundOption = document.getElementsByClassName('wound-option')
+    // let woundDropdown = document.getElementById('wound-dropdown').value
+    // console.log("woundDropdown", woundDropdown)
+    // // let woundOption = document.getElementsByClassName('wound-option')
     
 
-    let drainSitesDropdown = document.getElementById('drain-sites-dropdown').value
-    console.log("drainSitesDropdown", drainSitesDropdown)
+    // let drainSitesDropdown = document.getElementById('drain-sites-dropdown').value
+    // console.log("drainSitesDropdown", drainSitesDropdown)
 
-    let removalDate = document.getElementById('end-date').value
-    console.log("removalDate", removalDate)
+    // let removalDate = document.getElementById('end-date').value
+    // console.log("removalDate", removalDate)
 
-    let legWoundsDropdown = document.getElementById('leg-wounds-dropdown').value
-    console.log("legWoundsDropdown", legWoundsDropdown)
+    // let legWoundsDropdown = document.getElementById('leg-wounds-dropdown').value
+    // console.log("legWoundsDropdown", legWoundsDropdown)
 
-    // p1.innerHTML = dischargeDate + " " + microValue 
-    //         + " " + normalSwellingText + " " + woundDropdown 
-    //         + " " + drainSitesDropdown + " " + removalDate
-    //         + " " + legWoundsDropdown + " " + removalDate
-
-
-    if ((dischargeDate == "") || (woundDropdown.value == "select") ) {
-        alert("please enter the required fields")
+    // if ((dischargeDate == "") || (woundDropdown.value == "select") ) {
+    //     alert("please enter the required fields")
         
         
         
-    } else {
+    // } else {
 
-        p1.innerHTML = dischargeDate + " " + microValue 
-        + " " + normalSwellingText + " " + woundDropdown 
-        + " " + drainSitesDropdown + " " + removalDate
-        + " " + legWoundsDropdown + " " + removalDate
+    //     p1.innerHTML = dischargeDate + " " + microValue 
+    //     + " " + normalSwellingText + " " + woundDropdown 
+    //     + " " + drainSitesDropdown + " " + removalDate
+    //     + " " + legWoundsDropdown + " " + removalDate
 
-    }
-
-
-
-    doc.text('Mr J Blogg XX-XXX', 20, 80);
-    doc.text('NHS NUMBER XXX XXX XXX', 20, 90);
-
-
-    doc.addImage(checkedImg, 'JPEG', 280, 100, 150, 300);
-
-
-    doc.fromHTML($('#left-text').get(0), 20, 92, {
-        'width': 250,        
-    });
-
-    doc.fromHTML($('#bottom-text').get(0), 20, 410, {
-        'width': 400,
-    });
-
-    doc.fromHTML($('#bottom-sign').get(0), 20, 590, {
-        'width': 400,
-    });
-
-    // Save pdf
-    doc.save('Photo-at-Discharge.pdf');
-}
-// End PDF
+    // }
 
 
 
-//works - TODO: might not be necessary 
-function checkDrName() {
-
-    let docTextbox = document.getElementById('doctor-name')
-
-    if ((docTextbox).value == "") {
-        alert('Please enter a doctors name')
-    }
-}
-
-function checkWoundImages() {
-
-    if (document.getElementById('wound-photo-one').checked == true) {
-        checkedImg = document.getElementById('wound-photo-one-img');
-        // doc.addImage(checkedImg, 'JPEG', 280, 100, 150, 300);
-    } 
-    else if (document.getElementById('wound-photo-two').checked == true) {
-        checkedImg = document.getElementById('wound-photo-two-img');
-        // doc.addImage(checkedImg, 'JPEG', 280, 100, 150, 300);
-    } 
-    else if (document.getElementById('wound-photo-three').checked == true) {
-        checkedImg = document.getElementById('wound-photo-three-img');
-        // doc.addImage(checkedImg, 'JPEG', 280, 100, 150, 300);
-    }
-    else if (document.getElementById('wound-photo-four').checked == true) {
-        checkedImg = document.getElementById('wound-photo-four-img');
-        // doc.addImage(checkedImg, 'JPEG', 280, 100, 150, 300);
-    }
-    else if (document.getElementById('wound-photo-five').checked == true) {
-        checkedImg = document.getElementById('wound-photo-five-img');
-        // doc.addImage(checkedImg, 'JPEG', 280, 100, 150, 300);
-    }
-
-}
-
-
-
-
-function checkMicroForm() {
-
-    if (document.getElementById('micro-one').checked == true) {
-        // var checkedImg = document.getElementById('wound-photo-one-img');
-        // doc.addImage(checkedImg, 'JPEG', 280, 100, 150, 300);
-        microValue = document.getElementById('micro-one').value
-        console.log(microValue)
-    } 
-    else if (document.getElementById('micro-two').checked == true) {
-        // var checkedImg = document.getElementById('wound-photo-two-img');
-        // doc.addImage(checkedImg, 'JPEG', 280, 100, 150, 300);
-        microValue = document.getElementById('micro-two').value
-
-    } 
-    else if (document.getElementById('micro-three').checked == true) {
-        // checkedImg = document.getElementById('wound-photo-three-img');
-        // doc.addImage(checkedImg, 'JPEG', 280, 100, 150, 300);
-        microValue = document.getElementById('micro-two').value
-
-    }
-
-
-}
+*/
